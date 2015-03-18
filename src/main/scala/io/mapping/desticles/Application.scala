b67610c3219a1445a7241a009543e5f0e4d680a8
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2015-03-18 12:43:52 -0400

    Better default example

--- src/main/scala/io/mapping/desticles/Application.scala
+++ src/main/scala/io/mapping/desticles/Application.scala
@@ -1,42 +1,48 @@
 package io.mapping.desticles
 
 import com.typesafe.config.ConfigFactory
-import io.mapping.desticles.http.BungieHttpProvider
+import io.mapping.desticles.controller.{CharacterController, InventoryController, PlayerAccountController}
 import org.json4s.DefaultFormats
-import org.json4s.native.Serialization.writePretty
 
 object Application extends App {
 	val config = ConfigFactory.load()
 
 	implicit val formats = DefaultFormats
 
-	val playerObject = BungieHttpProvider.searchDestinyPlayer(config.getString("playerName"))
-	//println(writePretty(playerObject))
+	/**
+	 * Get a player and their account
+	 */
+	val playerAcct = PlayerAccountController.getPlayerAccount(config.getString("playerName"))
+	println(s"${playerAcct.player.displayName}'s membership id is ${playerAcct.account.data.membershipId}")
 
-	val account = BungieHttpProvider.getAccount(playerObject)
-	//println(writePretty(account))
+	/**
+	 * Get the player's top-ranked character
+	 */
+	val topChar = playerAcct.account.data.characters.sortBy(- _.characterLevel).head
+	println(s"The first top-ranked character, character id ${topChar.characterBase.characterId}, is level ${topChar.characterLevel}; their percent to next level is ${topChar.percentToNextLevel}")
 
-	val topChar = account.data.characters.filter(pc => pc.characterLevel == 32)
+	/**
+	 * Get stats and progression on this character
+	 */
+	val topCharStats = CharacterController.getCharacterStats(playerAcct.player, topChar)
+	println(s"This character has ${topCharStats.merged.allTime.kills.get.basic.displayValue} kills")
 
-	for (c <- account.data.characters) {
-		println("For character:")
-		println(writePretty(c))
+	val topCharProg = CharacterController.getCharacterProgression(playerAcct.player, topChar)
+	val sumDailyProgs = topCharProg.data.progressions.getOrElse(Array()).foldLeft(0L)((b, p) => b + p.dailyProgress.getOrElse(0L))
+	val avgProgs = sumDailyProgs / topCharProg.data.progressions.getOrElse(Array()).length
+	println(s"Their average daily progress is $avgProgs")
 
-		val i = BungieHttpProvider.getInventory(playerObject, c)
+	/**
+	 * Get the character's currency, and the best item from the character's inventory and its detail
+	 */
+	val inv = InventoryController.getCharacterInventory(playerAcct.player, topChar).data
+	var sumCurr = inv.currencies.foldLeft(0L)((b, idc) => b + idc.value)
+	println(s"They have a total of $sumCurr currency")
 
-		println("Inventory:")
-		println(writePretty(i))
+	val topItem = inv.buckets.Equippable.flatMap(idb => idb.items).sortBy(- _.qualityLevel).head
+	val topItemDetail = InventoryController.getInventoryItemDetail(playerAcct.player, topChar, topItem.itemInstanceId)
+	println(s"The top quality item, instance id ${topItem.itemInstanceId}, is of quality level ${topItem.qualityLevel} and has the following perks:")
+	for (p <- topItemDetail.data.item.perks) {
+		println(s"\t${p.perkHash}")
 	}
-
-	//println("The top char is")
-	//println(writePretty(topChar))
-
-//	println(
-//		writePretty(
-//			BungieHttpProvider.stringGet(
-//				Seq(playerObject.membershipType.toString, "Account", playerObject.membershipId, "Character", topChar.head.characterBase.characterId, "Inventory"),
-//				Some(Seq("definitions=True"))
-//			)
-//		)
-//	)
 }
