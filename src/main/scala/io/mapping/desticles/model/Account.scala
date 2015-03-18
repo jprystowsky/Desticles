3f9cf08c35ac55374479f1f5b5f5c8fbb856696f
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2015-03-18 09:51:19 -0400

    Formatting

--- src/main/scala/io/mapping/desticles/model/Account.scala
+++ src/main/scala/io/mapping/desticles/model/Account.scala
@@ -1,11 +1,15 @@
 package io.mapping.desticles.model
 
-case class Account(data: AccountData)
+case class Account(
+	                  data: AccountData
+	                  )
 
-case class AccountData(membershipId: String,
-                       membershipType: Int,
-                       characters: Array[PlayerCharacter],
-                       clanName: String,
-                       clanTag: String,
-                       inventory: PlayerInventory,
-                       grimoireScore: Long)
+case class AccountData(
+	                      membershipId: String,
+	                      membershipType: Int,
+	                      characters: Array[PlayerCharacter],
+	                      clanName: String,
+	                      clanTag: String,
+	                      inventory: PlayerInventory,
+	                      grimoireScore: Long
+	                      )
