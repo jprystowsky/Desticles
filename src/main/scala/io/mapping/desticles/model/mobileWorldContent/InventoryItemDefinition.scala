8abc2176676dcc4f901377c0c8428932e23025ed
Jacob Prystowsky <jprystowsky@ohio-cpa.com>
2015-03-20 14:41:53 -0400

    Rm unused class

--- src/main/scala/io/mapping/desticles/model/mobileWorldContent/InventoryItemDefinition.scala
+++
@@ -1,7 +0,0 @@
-package io.mapping.desticles.model.mobileWorldContent
-
-// todo: flush out with rest
-case class InventoryItemDefinition(
-	                                  itemHash: Long,
-	                                  itemName: Option[String]
-	                                  )
